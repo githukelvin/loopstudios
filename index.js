25b5b653b8b5f1875a3f10dd75c4a7133b1b8269
githukelvin <kelvingithu019@gmail.com>
2022-10-27 08:51:09 +0300

    nav bar functionality

---
+++ index.js
@@ -0,0 +1,21 @@
+const ham = document.querySelector(".ham");
+const ul = document.querySelector(".nav-bar nav ul");
+
+// console.log(ham.getAttribute("data-visible"))
+// console.log()
+
+ham.addEventListener("click", () => {
+const visible = ul.getAttribute("data-visible");
+
+if(visible=='false'){
+    ul.setAttribute("data-visible",true);
+    ham.setAttribute("data-visible",true);
+}else if (visible=='true'){
+    ul.setAttribute("data-visible",false)
+    ham.setAttribute("data-visible",false)
+}
+
+console.log(visible)
+
+
+});
